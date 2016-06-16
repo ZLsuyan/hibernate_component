1b3a29809980d440191aa3b446284d6b269d7e09
ZLsuyan <18804620682@163.com>
2016-06-16 15:29:48 +0800

    init commit

---
+++ hibernate_component/src/com/hit/hibernate/Wife.java
@@ -0,0 +1,27 @@
+package com.hit.hibernate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.IdClass;
+
+
+public class Wife {
+
+	private String wifeName;
+	private int age;
+
+	public String getWifeName() {
+		return wifeName;
+	}
+	public void setWifeName(String wifeName) {
+		this.wifeName = wifeName;
+	}
+	
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+}
